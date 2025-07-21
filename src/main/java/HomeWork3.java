035124f2025d74769ab4132788d909df5df84da4
DmitriyStavskiy <Stavskiydv@inbox.ru>
2025-07-21 23:11:31 +0300

    finaly ver.

---
+++ src/main/java/HomeWork3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class HomeWork3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("ведите ваше имя;");
+        String name = sc.toString();
+        System.out.println("Привет, " + name + "!");
+    }
+}
